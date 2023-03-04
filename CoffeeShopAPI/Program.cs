9750399ee7cef787fce4cd2f617b685db191b458
Trevor Benit <trevordb10@gmail.com>
2023-03-04 18:22:57 -0500

    Add swagger usage

--- CoffeeShopAPI/Program.cs
+++ CoffeeShopAPI/Program.cs
@@ -1,10 +1,25 @@
-// Create and configure app builder
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+var builder = WebApplication.CreateBuilder(args);
 
-// Actually build and run application
-WebApplication app = builder.Build();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
