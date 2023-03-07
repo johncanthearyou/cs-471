bc74b7762b0cbcad19fbbb59bb53acc16f3cb888
Trevor Benit <trevordb10@gmail.com>
2023-03-07 17:03:01 -0500

    Update transactions to include item list

--- coffee-shop-db/create-transaction-table.sql
+++ coffee-shop-db/create-transaction-table.sql
@@ -1,8 +1,8 @@
+DROP TABLE transactions;
+
 CREATE TABLE transactions
 (
   id INTEGER primary key AUTOINCREMENT,
-  drinks json,
-  food json,
   payment text,
   total_cost float not null,
   date datetime DEFAULT CURRENT_TIMESTAMP,
